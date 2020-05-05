45eb118b1d77dba9aaca6e6ca5a070955c6148ae
Serdar Karacay <csharp221@gmail.com>
2020-05-05 07:44:35 +0300

    submodule bug fix

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="example">
+    <button v-on:click="callBackendApi">Call Api Backend</button>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "example",
+  methods: {
+    async callBackendApi() {
+      debugger;
+      const req = await axios.get("/api");
+      alert(req.data);
+    },
+  },
+};
+</script>
